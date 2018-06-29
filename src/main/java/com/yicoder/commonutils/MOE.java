4be35139d2714d2981a10b7857bb3351dd8d7ab3
ziping <ziping@suneee.com>
2018-06-29 17:38:07 +0800

    Revert "revert测试4"
    This reverts commit 26a3b3d

--- src/main/java/com/yicoder/commonutils/MOE.java
+++ src/main/java/com/yicoder/commonutils/MOE.java
@@ -92,7 +92,8 @@ public class MOE {
     public static void main(String[] args) {
         long starttime = System.currentTimeMillis();
         System.out.println("蚁群算法开始：");
-
+        System.out.println("蚁群算法开始：");
+        System.out.println("蚁群算法开始：");
         System.out.println("starttime:" + starttime);
 
 //        int[][] cityDot = new int[][] {
