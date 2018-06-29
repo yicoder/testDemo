8cbe2fc5b2a78c216d2e18abe3b2d537dec27142
ziping <ziping@suneee.com>
2018-06-29 17:12:04 +0800

    Revert "测试revert"
    This reverts commit ad86bba

--- src/main/java/com/yicoder/commonutils/MOE.java
+++ src/main/java/com/yicoder/commonutils/MOE.java
@@ -91,8 +91,6 @@ public class MOE {
     // 使用穷举法结算
     public static void main(String[] args) {
         long starttime = System.currentTimeMillis();
-        System.out.println("蚁群算法开始：");
-
         System.out.println("starttime:" + starttime);
 
 //        int[][] cityDot = new int[][] {
